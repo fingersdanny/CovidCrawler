349e4b1da026e686ff12124a563ecf6bd96f6524
isevou__ <kbry12@naver.com>
2023-10-08 14:49:14 +0900

    refactor : Delete CovidStatusUtility.java and commit first draft of CovidScaper.java

--- src/main/java/covid_crawler/src/CovidScraper.java
+++ src/main/java/covid_crawler/src/CovidScraper.java
@@ -2,19 +2,13 @@ package covid_crawler.src;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CovidScraper {
-    private static String[] cities = {"I11", "I21", "I22", "I23", "I24"
-                                    , "I25", "I26", "I29", "I31", "I32"
-                                    , "I33", "I34", "I35", "I36", "I37"
-                                    , "I38", "I39"};
-
-    private static int[][] coordinates = {{150, 70}, {522, 486}, {656, 491}};
     private static String[] cityIds = {"V11", "V21", "V22", "V23", "V24"
                                     , "V25", "V26", "V29", "V31", "V32"
                                     , "V33", "V34", "V35", "V36", "V37"
@@ -24,55 +18,65 @@ public class CovidScraper {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
+        List<CovidStatus> covidStatusList = new ArrayList<>();
 
-        int now = 0;
+        covidStatusList.add(getCovidStatus(driver));
         for (String cityIndex : cityIds) {
-            System.out.println("cityIndex = " + cityIndex);
+
             try {
                 driver.findElement(By.id(cityIndex)).click();
-                String city = driver.findElement(By.id("regionname")).getText();
-                System.out.println(city);
-                driver.findElement(By.id("byDateTab")).click();
-            } catch (ElementClickInterceptedException e) {
-//                e.printStackTrace();
-                try {
-                    Point origin = driver.findElement(By.className("Map")).getLocation();
-                    System.out.println("x = " + origin.x);
-                    System.out.println("y = " + origin.y);
-                    Actions actions = new Actions(driver);
-                    Point location = driver.findElement(By.id(cityIndex)).getLocation();
-                    int x = location.x;
-                    int y = location.y;
-                    boolean find = false;
-                    for (int i = 0; i < coordinates[now][0]; i++) {
-                        for (int j = 0; j < coordinates[now][1]; j++) {
-                            y++;
-                            actions.moveToLocation(x, y).click();
-                            String city = driver.findElement(By.id("regionname")).getText();
-                            System.out.println("x = " + x);
-                            System.out.println("y = " + y);
-                            if (city.equals("인천")) {
-                                find = true;
-                                break;
-                            }
-                        }
-                        x++;
-                        if (find) {
-                            break;
-                        }
-                        y = location.y;
-                    }
-                    System.out.println("loc x = " + x);
-                    System.out.println("loc y = " + y);
-                    actions.moveToLocation(x, y).click();
-                    String city = driver.findElement(By.id("regionname")).getText();
-                    System.out.println(city);
-                    now++;
-                } catch (ElementClickInterceptedException e1) {
-                    e.printStackTrace();
+                CovidStatus curCovidStatus = getCovidStatus(driver);
+                if (curCovidStatus != null) {
+                    covidStatusList.add(curCovidStatus);
                 }
+            } catch (ElementClickInterceptedException e) {
+                e.printStackTrace();
             }
         }
+        for (CovidStatus cs : covidStatusList) {
+            System.out.println(cs.toString());
+        }
         driver.close();
+    }
+
+    private static CovidStatus getCovidStatus(WebDriver driver) {
+        try {
+            String city = driver.findElement(By.id("regionname")).getText();
+
+            Thread.sleep(3000);
+
+            driver.findElement(By.id("byDateTab")).click();
+            String dt = driver.findElement(By.id("commaconfirmval1"))
+                    .findElement(By.className("Color1")).getText().replaceAll(",", "");
+            String dD = driver.findElement(By.id("commadeadval1"))
+                    .findElement(By.className("Color3")).getText().replaceAll(",", "");
+            int dailyTotal = Integer.parseInt(dt);
+            int dailyDeceased = Integer.parseInt(dD);
+
+            Thread.sleep(3000);
+
+            driver.findElement(By.id("byPathTab")).click();
+            String dO = driver.findElement(By.id("commadomesticval"))
+                    .findElement(By.className("Color1")).getText().replaceAll(",", "");
+            String fO = driver.findElement(By.id("commaoverseasval"))
+                    .findElement(By.className("Color3")).getText().replaceAll(",", "");
+            int domesticOriented = Integer.parseInt(dO);
+            int foreignOriented = Integer.parseInt(fO);
+
+            Thread.sleep(3000);
+
+            driver.findElement(By.id("allTab")).click();
+            String tC = driver.findElement(By.id("commaconfirmval"))
+                    .findElement(By.className("Color1")).getText().replaceAll(",", "");
+            String tD = driver.findElement(By.id("commadeadval"))
+                    .findElement(By.className("Color3")).getText().replaceAll(",", "");
+            int totalConfirmed = Integer.parseInt(tC);
+            int totalDeceased = Integer.parseInt(tD);
+
+        return new CovidStatus(city, dailyTotal, dailyDeceased, domesticOriented, foreignOriented, totalConfirmed, totalDeceased);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
